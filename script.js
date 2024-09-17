0baffa98058fb92755d84333e83e4c74faa636ee
bielbm <163012253+bielbm@users.noreply.github.com>
2024-09-17 08:28:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista"
+
+var paulo = document.querySelector('#linhaPaulo');
+var pesoPaulo = document.querySelector('.info-peso').textContent;
+var alturaPaulo = document.querySelector('.info-altura').textContent;
+var imcPaulo = pesoPaulo / (alturaPaulo * alturaPaulo);
+paulo.querySelector(".info-imc").textContent= imcPaulo;
